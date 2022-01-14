8a899257013662fc1a1186a2fe9daadb3723a55d
AlexGrin1 <a_grynevich@mail.ru>
2022-01-14 17:09:37 +0300

    Feat: devided into widget

--- weather_app/lib/screens/weatherScreen.dart
+++ weather_app/lib/screens/weatherScreen.dart
@@ -24,81 +24,138 @@ class _WeatherScreenState extends State<WeatherScreen> {
       appBar: AppBar(
         title: Text(_state.userCityValue),
       ),
-      body: Stack(children: [
-//         Image.network(
-        // 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.sat24.com%2Fen&psig=AOvVaw0lgfi2RsRejzX4fE386F0f&ust=1642250083715000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCICM776gsfUCFQAAAAAdAAAAABAD'),
-        FutureBuilder<WeatherList>(
-          future: weather,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemExtent: 200,
-                  shrinkWrap: true,
-                  // scrollDirection: Axis.horizontal,
-                  itemCount: snapshot.data?.forecast?.forecastday?.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      color: Colors.blue[100],
-                      margin: EdgeInsets.only(
-                          top: 10, right: 40, bottom: 10, left: 40),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                  '${snapshot.data?.forecast?.forecastday?[index].date}'),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Image.network(
-                                          'https:${snapshot.data?.forecast?.forecastday?[index].day?.condition?.icon}'),
-                                      Text(
-                                          '${snapshot.data?.forecast?.forecastday?[index].day?.condition?.text}'),
-                                    ],
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 16.0),
-                                    child: Text(
-                                      '${snapshot.data?.forecast?.forecastday?[index].day?.maxtempC?.round()} \u2103',
-                                      style: TextStyle(fontSize: 30),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Card(
-                                    color: Colors.amber,
-                                    child: Column(
-                                      children: [
-                                        Text(
-                                            'Wind: ${snapshot.data?.forecast?.forecastday?[index].day?.maxwindKph?.round()} kp/h'),
-                                        Text(
-                                            'Precipitation: ${snapshot.data?.forecast?.forecastday?[index].day?.avghumidity} %'),
-                                      ],
-                                    ),
-                                  ),
-                                  Text(
-                                      'Humidity: ${snapshot.data?.forecast?.forecastday?[index].day?.dailyChanceOfRain} %'),
-                                ],
-                              ),
-                            ]),
-                      ),
-                    );
-                  });
-            } else if (snapshot.hasError) {
-              return Text('Error');
-            }
-            return Center(child: CircularProgressIndicator());
-          },
-        ),
-      ]),
+      body: ListView(
+        children: <Widget>[
+          FutureBuilder<WeatherList>(
+            future: weather,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    CurentWeather(snapshot.data?.current),
+                    Container(child: FutureWeather(snapshot))
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text('Error');
+              }
+              return Center(child: CircularProgressIndicator());
+            },
+          )
+        ],
+      ),
     );
+  }
+}
+
+class CurentWeather extends StatelessWidget {
+  var snapshot;
+  CurentWeather(this.snapshot, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.blue[100],
+      margin: EdgeInsets.all(10),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                        'https:${snapshot.condition?.icon}',
+                        scale: 0.8,
+                      ),
+                      Text('${snapshot?.condition?.text}'),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Text(
+                      '${snapshot?.tempC?.round()} \u2103',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+      ),
+    );
+    ;
+  }
+}
+
+class FutureWeather extends StatelessWidget {
+  var snapshot;
+  FutureWeather(this.snapshot, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        // itemExtent: 200,
+        shrinkWrap: true,
+        // scrollDirection: Axis.horizontal,
+        itemCount: snapshot.data?.forecast?.forecastday?.length,
+        itemBuilder: (context, index) {
+          return Card(
+            color: Colors.blue[100],
+            margin: EdgeInsets.all(10),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                        '${snapshot.data?.forecast?.forecastday?[index].date}'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.network(
+                              'https:${snapshot.data?.forecast?.forecastday?[index].day?.condition?.icon}',
+                              scale: 0.8,
+                            ),
+                            Text(
+                                '${snapshot.data?.forecast?.forecastday?[index].day?.condition?.text}'),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16.0),
+                          child: Text(
+                            '${snapshot.data?.forecast?.forecastday?[index].day?.maxtempC?.round()} \u2103',
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                                'Wind: ${snapshot.data?.forecast?.forecastday?[index].day?.maxwindKph?.round()} kp/h'),
+                            Text(
+                                'Precipitation: ${snapshot.data?.forecast?.forecastday?[index].day?.avghumidity} %'),
+                          ],
+                        ),
+                        Text(
+                            'Humidity: ${snapshot.data?.forecast?.forecastday?[index].day?.dailyChanceOfRain} %'),
+                      ],
+                    ),
+                  ]),
+            ),
+          );
+        });
   }
 }
